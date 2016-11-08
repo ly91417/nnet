00fa6e1e0450aee223c677c3906b5267462786e0
Ying Li <ly91417@gmail.com>
2016-11-08 10:44:32 -0600

    first commit

---
+++ nnet/src/nnet/NodeWeightPair.java
@@ -0,0 +1,13 @@
+package nnet;
+public class NodeWeightPair{
+	public Node node; //The parent node
+	public Double weight; //Weight of this connection
+	
+	//Create an object with a given parent node 
+	//and connect weight
+	public NodeWeightPair(Node node, Double weight)
+	{
+		this.node=node;
+		this.weight=weight;
+	}
+}
